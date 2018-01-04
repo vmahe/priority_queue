db177f90657c2cd33d820fbac6c688d006aad467
vmahe <35092404+vmahe@users.noreply.github.com>
2018-01-03 23:20:00 -0800

    Update priority_queue.h

--- priority_queue.h
+++ priority_queue.h
@@ -15,6 +15,15 @@ typedef struct priority_queue_t_ {
     int *q;
 } priority_queue_t;
 
+priority_queue_t *pq_init(bool);
+void pq_destroy(priority_queue_t *);
+bool pq_is_empty(priority_queue_t *);
+int pq_get_size(priority_queue_t *);
+int pq_get_count(priority_queue_t *);
+int pq_peek(priority_queue_t *);
+void pq_push(priority_queue_t *, int);
+int pq_pop(priority_queue_t *);
+
 static inline void swap (int *a, int *b)
 {
     int tmp = *a;
